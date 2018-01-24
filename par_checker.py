3cc3106ee61e861a3cbb177b4489790fc0547559
ShawHsing <cqyyxiaoxincooi@gmail.com>
2018-01-24 15:53:52 +0800

    python learning

---
+++ par_checker.py
@@ -0,0 +1,31 @@
+from Stack import Stack #import the Stack class as perviously defined
+
+# Completed extended par_check for:{,[(,),],}
+def par_checker(symbol_string):
+    s = Stack()
+    balanced = True
+    index = 0
+    while index < len(symbol_string) and balanced:
+        symbol = symbol_string[index]
+        if symbol in "([{":
+            s.push(symbol)
+        else:
+            if s.is_empty():
+                balanced = False
+            else:
+                top = s.pop()
+                if not matches(top, symbol):
+                    balanced = False
+
+        index = index + 1
+
+    if balanced and s.is_empty():
+        return True
+    else:
+        return False
+
+def matches(open, close):
+    opens = "([{"
+    closes = ")]}"
+    return opens.index(open) == closes.index(close)
+
