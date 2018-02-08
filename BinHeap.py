b3c518bd1e6a06a4497f9c8c10a35ffebab05944
ShawHsing <cqyyxiaoxincooi@gmail.com>
2018-02-08 23:30:31 +0800

    Python learning

--- BinHeap.py
+++ BinHeap.py
@@ -11,4 +11,40 @@ class BinHeap:
         self.heap_list.append(k)
         self.current_size = self.current_size + 1
         self.perc_up(self.current_size)
-    
+    def perc_down(self, i):
+        while (i * 2) <= self.current_size:
+            mc = self.min_child(i)
+            if self.heap_list[i] > self.heap_list[mc]:
+                self.heap_list[mc], self.heap_list[i] = self.heap_list[i], self.heap_list[i // 2]
+            i = mc
+    def min_child(self, i):
+        if i * 2 + 1 > self.current_size:
+            return i * 2
+        else:
+            if self.heap_list[i *2] < self.heap_list[i * 2 + 1]:
+                return i * 2
+            else:
+                return i * 2 + 1
+    def del_min(self):
+        ret_val = self.heap_list[1]
+        self.heap_list[1] = self.heap_list[self.current_size]
+        self.current_size = self.current_size - 1
+        self.heap_list.pop()
+        self.perc_down(1)
+        return  ret_val
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
