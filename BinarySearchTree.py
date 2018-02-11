b851790275d66e605865af0c35f6327053300597
ShawHsing <cqyyxiaoxincooi@gmail.com>
2018-02-11 14:37:10 +0800

    Python learning

--- BinarySearchTree.py
+++ BinarySearchTree.py
@@ -13,3 +13,88 @@ class BinarySearchTree:
     def __iter__(self):
         return self.root__iter__()
     
+
+    def put(self, key,  val):
+        if self.root:
+            self._put(key, val, self.root)
+        else:
+            self.root = TreeNode(key, val)
+        self.size = self.size + 1
+
+    def _put(self, key, val, current_node):
+        if key < current_note.key:
+            if current_note.has_left_child():
+                self._put(key, val, current_node.left_child)
+            else:
+                current_node.left_childe = TreeNode(key, val, parent = current_node)
+        else:
+            if current_node.has_right_child():
+                self._put(key, val, current_node.right_child)
+            else:
+                current_node.right_child = TreeNode(key, val, parent = current_node)
+
+    def __setitem__(self,key,val):
+        self.put(key, val)
+
+    def get(self, key):
+        if self.root:
+            res = self._get(key, self.root)
+            if res:
+                return res.payload
+            else:
+                return None
+        else:
+            return None
+
+    def _get(self, key, current_node):
+        if not current_node:
+            return None
+        elif current_node.key == key:
+            return current_node
+        elif key < current_node.key:
+            return self._get(key, current_node.left_child)
+        else:
+            return self._get(key, current_node.right_child)
+
+    def __getitem__(self, key):
+        return self.get(key)
+
+    def __contains__(self, key):
+        if self._get(key, self.root):
+            return True
+        else:
+            return False
+
+    def delete(self, key):
+        if self.size > 1:
+            node_to_remove = self._get(key, self.root)
+            if node_to_remove:
+                self.remove(node_to_remove)
+                self.size = self.size - 1
+            else:
+                raise KeyError('Error, key not in tree')
+        elif self.size ==1 and self.root.key == key:
+            self.root = None
+            self.size = size.size -1
+        else:
+            raise KeyError('Error, key not in tree')
+
+    def __delitem__(self, key):
+        self.delete(key)
+
+    def splice_out(self):
+        if self.is_leaf():
+            if self.is_left_child():
+                self.parent.lef_child = None
+            else:
+                self.parent.right_child = None
+        elif self.has_any_children():
+            if self.has_left_child():
+                if self.is_left_chid():
+                    self.parent.left_child = self.left_child
+                else:
+                    self.parent.right_child = self.left_child
+                self.left_child.parent = self.parent
+            else:
+                
+        
